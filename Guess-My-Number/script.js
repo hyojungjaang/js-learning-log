3259ed8f38271af4826bc8732520a63948ac966a
hyojungjaang <gamutemu@gmail.com>
2025-04-12 12:55:58 +0900

    Selecting and Manipulating Elements

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -2,3 +2,11 @@
 
 // 특정 HTML 요소의 텍스트 내용을 콘솔에 출력하는 역할
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// With an input field to get the actual value, we use the value property
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
