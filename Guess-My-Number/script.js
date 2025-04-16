02d7cad8e3168a74abaf7257464fcfb8e8bb5ea4
hyojungjaang <gamutemu@gmail.com>
2025-04-16 20:56:04 +0900

    Handling Click Events

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // 특정 HTML 요소의 텍스트 내용을 콘솔에 출력하는 역할
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -10,3 +11,13 @@ document.querySelector('.score').textContent = 10;
 // With an input field to get the actual value, we use the value property
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
